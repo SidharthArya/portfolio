07d867af6ba079303c3f2acad0f9404f6610dcfd
Sidharth Arya <sidhartharya10@gmail.com>
2020-11-23 04:43:21 +0530

    initial

--- js/controllers-debug.js
+++ js/controllers-debug.js
@@ -17,8 +17,32 @@ const contributions = new Vue({
         packages: []
     },
     created() {
-        fetch("https://api.github.com/users/sidhartharya/repos")
+        fetch("https://api.github.com/users/sidhartharya/repos?sort=updated'")
             .then(response => response.json())
             .then(json => {
                 this.packages = json
+            })}})
+
+const stars = new Vue({
+    el: '#Stars',
+    data: {
+        stars: []
+    },
+    created() {
+        fetch("https://api.github.com/users/sidhartharya/starred?per_page=100'")
+            .then(response => response.json())
+            .then(json => {
+                this.stars = json
+            })}})
+
+const following = new Vue({
+    el: '#Following',
+    data: {
+        following: []
+    },
+    created() {
+        fetch("https://api.github.com/users/sidhartharya/following?per_page=100")
+            .then(response => response.json())
+            .then(json => {
+                this.following = json
             })}})
